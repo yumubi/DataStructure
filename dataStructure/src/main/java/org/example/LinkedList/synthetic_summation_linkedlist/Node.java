27718d75ffc1e5ad99dac89e0114d8d430717d9b
yumubi <shaman2077c@163.com>
2023-07-13 22:54:14 +0800

    :egg: 两个单链表生成相加链表

---
+++ dataStructure/src/main/java/org/example/LinkedList/synthetic_summation_linkedlist/Node.java
@@ -0,0 +1,10 @@
+package org.example.LinkedList.synthetic_summation_linkedlist;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
